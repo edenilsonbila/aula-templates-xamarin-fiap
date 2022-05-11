b2a21304a606c757edb6d849009c13e27b2bc88b
EdenilsonBila <edn_santos@hotmail.com>
2022-05-10 22:53:10 -0300

    Add project files.

---
+++ XF/XF/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AbsoluteClicked(object sender,EventArgs e)
+        {
+            //Abre uma nova view
+            Navigation.PushAsync(new View.AbsoluteLayoutView());
+
+        }
+
+        private void GridLayoutClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.GridLayoutView());
+        }
+
+        private void StackLayoutClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.StackLayoutView());
+        }
+
+        private void FlexLayoutClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.FlexLayoutView());
+        }
+
+        private void RelativeLayoutClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.RelativeLayoutView());
+        }
+    }
+}
---
+++ XF/XF/View/DinamicoView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DinamicoView : ContentPage
+    {
+        bool temaPadrao;
+        public DinamicoView()
+        {
+            InitializeComponent();
+
+            temaPadrao = true;
+            Resources["TextoEstiloDinamico"] = Resources["TextoAzul"];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (temaPadrao)
+            {
+                Resources["TextoEstiloDinamico"] = Resources["TextoVermelho"];
+                temaPadrao = false;
+            }
+            else
+            {
+                Resources["TextoEstiloDinamico"] = Resources["TextoAzul"];
+                temaPadrao = true;
+            }
+        }
+    }
+}
